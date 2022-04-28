cc6f4ec5d96dd3330e31f9cfbb9409aac9db01f8
c.qper <clara.kuper@posteo.de>
2022-04-28 10:08:11 +0200

    replace reshape by double brackets
    Co-authored-by: Pablo Prietz <pablo@prietz.org>

--- examples/linear_time_model.py
+++ examples/linear_time_model.py
@@ -51,5 +51,5 @@ cloud_gaze['timestamp [s]'] = cloud_gaze['timestamp [ns]'] * 1e-9
 
 # predict lsl time in seconds
 cloud_gaze['lsl_time [s]'] = time_mapper.predict(
-    cloud_gaze['timestamp [s]'].values.reshape(-1, 1)
+    cloud_gaze[['timestamp [s]']]
 )
