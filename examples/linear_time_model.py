4cf8a7549a096f33f30c210e8dfef93c7c2d6545
Clara Kuper <clara-kuper@web.de>
2022-05-05 10:00:23 +0200

    replace hardcoded column names with variable

--- examples/linear_time_model.py
+++ examples/linear_time_model.py
@@ -29,35 +29,36 @@ path_to_cloud_events = './cloud_recordings/events.csv'
 cloud_event_data = pd.read_csv(path_to_cloud_events)
 
 # transform cloud timestamps to seconds
-cloud_event_data['timestamp [s]'] = cloud_event_data['timestamp [ns]'] * 1e-9
+cloud_event_data[event_column_timestamp] = cloud_event_data['timestamp [ns]'] * 1e-9
 
 # filter events that were recorded in the lsl stream and in cloud
 name_intersection = np.intersect1d(
-    cloud_event_data['name'], lsl_event_data['time_series']
+    cloud_event_data[event_column_name], lsl_event_data[event_column_name]
 )
 
 # filter timestamps by the event intersection
 filtered_cloud_event_data = cloud_event_data[
-    cloud_event_data['name'].isin(name_intersection)
+    cloud_event_data[event_column_name].isin(name_intersection)
 ]
 
 filtered_lsl_event_data = lsl_event_data[
-    lsl_event_data['time_series'].isin(name_intersection)
+    lsl_event_data[event_column_name].isin(name_intersection)
 ]
 
 # fit a linear model
 time_mapper = linear_model.LinearRegression()
 time_mapper.fit(
-    filtered_cloud_event_data[['timestamp [s]']], filtered_lsl_event_data['time_stamps']
+    filtered_cloud_event_data[[event_column_timestamp]],
+    filtered_lsl_event_data[event_column_timestamp]
 )
 
 # use convert gaze time stamps from cloud to lsl time
 cloud_gaze_data = pd.read_csv('./cloud_recordings/gaze.csv')
 
 # map from nanoseconds to seconds
-cloud_gaze_data['timestamp [s]'] = cloud_gaze_data['timestamp [ns]'] * 1e-9
+cloud_gaze_data[event_column_timestamp] = cloud_gaze_data['timestamp [ns]'] * 1e-9
 
 # predict lsl time in seconds
 cloud_gaze_data['lsl_time [s]'] = time_mapper.predict(
-    cloud_gaze_data[['timestamp [s]']]
+    cloud_gaze_data[[event_column_timestamp]]
 )
