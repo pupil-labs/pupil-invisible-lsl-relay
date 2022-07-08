912605aac226fea21460950c57a2574013638f40
Clara Kuper <clara-kuper@web.de>
2022-07-08 12:00:16 +0200

    change sanity checks, re-structure loading

--- src/pupil_labs/invisible_lsl_relay/xdf_cloud_time_sync.py
+++ src/pupil_labs/invisible_lsl_relay/xdf_cloud_time_sync.py
@@ -1,33 +1,22 @@
 import json
-import os.path
+import logging
 import pathlib
 
 import click
 import pyxdf
 
 
+logger = logging.getLogger(__name__)
+
+
 @click.command()
-@click.argument('path_to_xdf', nargs=1)
-@click.argument('paths_to_exports', nargs=-1)
+@click.argument('path_to_xdf', nargs=1, type=click.Path(exists=True))
+@click.argument('paths_to_exports', nargs=-1, type=click.Path(exists=True))
 def main(path_to_xdf, paths_to_exports):
-    check_input(path_to_xdf, paths_to_exports)
+    if len(paths_to_exports) == 0:
+        logger.info('No paths to exports provided. Looking inside current directory.')
+        paths_to_exports = ['.']
     load_files(path_to_xdf, paths_to_exports)
-
-
-def check_input(path_to_xdf, paths_to_export):
-    if not pathlib.Path(path_to_xdf).is_file():
-        # path_to_xdf should be a file with the ending .xdf
-        raise FileNotFoundError(f'The path {path_to_xdf} does not point to a file.')
-    if not path_to_xdf.endswith('.xdf'):
-        raise TypeError(f'The file {path_to_xdf} is not an .xdf file.')
-
-    for path in paths_to_export:
-        # each path to an export should be a valid path
-        if not pathlib.Path(path).is_dir():
-            raise NotADirectoryError(
-                'All paths provided after the path '
-                'to the xdf file must be valid directories.'
-            )
 
 
 def load_files(path_to_xdf, paths_to_export):
@@ -36,10 +25,13 @@ def load_files(path_to_xdf, paths_to_export):
         xdf_head, xdf_data = pyxdf.load_xdf(path_to_xdf)
     except Exception:
         raise IOError(f"Invalid xdf file {path_to_xdf}")
+    # extract all serial numbers from gaze streams
+    xdf_serial_nums = extract_serial_num_from_xdf(xdf_head)
+    # check cloud export paths
     for path in paths_to_export:
         for info_path in pathlib.Path(path).rglob("info.json"):
-            if check_files(xdf_head, info_path):
-                perform_time_alignment(path_to_xdf, os.path.dirname(info_path))
+            if json_serial_in_xdf(info_path, xdf_serial_nums):
+                perform_time_alignment(path_to_xdf, info_path.parent)
 
 
 def perform_time_alignment(xdf_path, recording_dir):
@@ -47,22 +39,38 @@ def perform_time_alignment(xdf_path, recording_dir):
     save_files()
 
 
-def check_files(head, path):
-    for x in head:
-        if x['info']['type'][0] == 'Gaze':
-            xdf_camera_serial = x['info']['desc'][0]['acquisition'][0][
-                'world_camera_serial'
-            ][0]
-            with open(path) as file:
-                data = json.load(file)
-                try:
-                    cloud_camera_serial = data['scene_camera_serial_number']
-                    if cloud_camera_serial == xdf_camera_serial:
-                        return True
-                except KeyError:
-                    # this is not a pupil info file
-                    pass
+def json_serial_in_xdf(path_infojson, xdf_serial_nums):
+    with open(path_infojson) as infojson:
+        data_infojson = json.load(infojson)
+        if extract_serial_num_from_infojson(data_infojson) in xdf_serial_nums:
+            return True
     return False
+
+
+def extract_serial_num_from_xdf(xdf_head):
+    camera_serial_nums = []
+    for x in xdf_head:
+        try:
+            if x['info']['type'][0] == 'Gaze':
+                xdf_camera_serial = x['info']['desc'][0]['acquisition'][0][
+                    'world_camera_serial'
+                ][0]
+                camera_serial_nums.append(xdf_camera_serial)
+        except KeyError as e:
+            logger.error("The xdf file does not contain the expected fields."
+                         "Make sure it's been streamed with the pupil invisible"
+                         "lsl relay version 2.1.0 or higher.")
+            raise e
+    return camera_serial_nums
+
+
+def extract_serial_num_from_infojson(data_infojson):
+    try:
+        cloud_camera_serial = data_infojson['scene_camera_serial_number']
+        return cloud_camera_serial
+    except KeyError:
+        # this is not a pupil info file
+        return None
 
 
 def save_files():
