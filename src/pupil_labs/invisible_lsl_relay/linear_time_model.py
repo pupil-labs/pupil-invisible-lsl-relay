82367a5dfadfa96a6a951e2865b271c14363a67d
Clara Kuper <clara-kuper@web.de>
2022-07-15 11:07:13 +0200

    perform time alignment

---
+++ src/pupil_labs/invisible_lsl_relay/linear_time_model.py
@@ -0,0 +1,153 @@
+# imports for the full pipeline
+import logging
+import numpy as np
+import pandas as pd
+import pathlib
+import pyxdf
+from sklearn import linear_model
+
+logger = logging.getLogger(__name__)
+
+event_column_name = 'name'
+event_column_timestamp = 'timestamp [s]'
+
+
+def perform_time_alignment(xdf_path, cloud_path, xdf_serial_num):
+    """
+    :param xdf_path: pathlib path pointing to the xdf file with all data streams
+    :param cloud_path: pathlib path pointing to the directory with all cloud recordings
+    :param xdf_serial_num: string specifying the serial number to look for in the xdf file
+    :return:
+    cloud_aligned_time - pandas data frame from cloud_path/gaze.csv with an
+    additional column for the mapped lsl time
+    time_mapper - linear regression model mapping time from xdf to cloud
+    """
+    logger.debug(f'performing time alignment between {xdf_path} and {cloud_path}')
+
+    filtered_xdf_data, filtered_cloud_data = get_filtered_events(
+        xdf_path,
+        cloud_path,
+        xdf_serial_num
+    )
+
+    cloud_aligned_time, cloud_to_lsl, lsl_to_cloud = fit_linear_model(
+        filtered_xdf_data,
+        filtered_cloud_data,
+        cloud_path
+    )
+
+    return cloud_aligned_time, cloud_to_lsl, lsl_to_cloud
+
+
+def get_filtered_events(xdf_path, cloud_path, xdf_serial_num):
+    xdf_event_data = load_xdf_events(
+        xdf_path,
+        world_camera_serial_num=xdf_serial_num
+    )
+    cloud_event_data = load_cloud_data(cloud_path)
+
+    filtered_cloud_data, filtered_xdf_data = filter_common_events(
+        xdf_event_data,
+        cloud_event_data
+    )
+
+    return filtered_cloud_data, filtered_xdf_data
+
+
+def load_xdf_events(path_to_xdf_recording, world_camera_serial_num):
+    stream_type = 'Event'
+    data, header = pyxdf.load_xdf(path_to_xdf_recording, select_streams=[
+        {'type': stream_type}
+    ])
+
+    # need to extract event screen with the correct scene camera
+    for event_stream in data:
+        if event_stream['info']['desc'][0]['acquisition'][0][
+            'world_camera_serial'
+        ][0] == world_camera_serial_num:
+            return xdf_event_to_df(event_stream)
+    raise ValueError(f'None of streams in {path_to_xdf_recording} contained '
+                     f'the serial number {world_camera_serial_num}!')
+
+
+def xdf_event_to_df(event_stream, column_name=event_column_name,
+                    column_timestamp=event_column_timestamp):
+    lsl_event_data = pd.DataFrame(columns=[column_name, column_timestamp])
+    lsl_event_data[column_name] = [name[0] for name in event_stream['time_series']]
+    lsl_event_data[column_timestamp] = event_stream['time_stamps']
+
+    return lsl_event_data
+
+
+def load_cloud_data(cloud_path, column_timestamp=event_column_timestamp):
+    cloud_event_data = load_df_from_dir(cloud_path, 'events.csv')
+    # transform cloud timestamps to seconds
+    cloud_event_data[column_timestamp] = cloud_event_data['timestamp [ns]'] * 1e-9
+
+    return cloud_event_data
+
+
+def filter_common_events(cloud_data,
+                         xdf_data, column_name=event_column_name):
+
+    name_intersection = np.intersect1d(
+        cloud_data[column_name], xdf_data[column_name]
+    )
+
+    # filter timestamps by the event intersection
+    filtered_cloud_data = cloud_data[
+        cloud_data[column_name].isin(name_intersection)
+    ]
+    filtered_xdf_data = xdf_data[
+        xdf_data[column_name].isin(name_intersection)
+    ]
+
+    return filtered_cloud_data, filtered_xdf_data
+
+
+def fit_linear_model(filtered_cloud_data,
+                     filtered_xdf_data,
+                     cloud_path,
+                     file_name='gaze.csv',
+                     column_timestamp=event_column_timestamp):
+
+    cloud_to_lsl_mapper = linear_time_mapper(filtered_cloud_data[[column_timestamp]],
+                                             filtered_xdf_data[column_timestamp])
+    lsl_to_cloud_mapper = linear_time_mapper(filtered_xdf_data[[column_timestamp]],
+                                             filtered_cloud_data[column_timestamp])
+    # load the file with the gaze data
+    cloud_gaze_data = load_df_from_dir(cloud_path, file_name)
+    # map from nanoseconds to seconds
+    cloud_gaze_data[column_timestamp] = cloud_gaze_data['timestamp [ns]'] * 1e-9
+
+    # map cloud time to lsl time in seconds
+    cloud_gaze_data['lsl_time [s]'] = cloud_to_lsl_mapper.predict(
+        cloud_gaze_data[[column_timestamp]]
+    )
+
+    return cloud_gaze_data, cloud_to_lsl_mapper, lsl_to_cloud_mapper
+
+
+def linear_time_mapper(x, y):
+    mapper = linear_model.LinearRegression()
+    mapper.fit(x, y)
+
+    return mapper
+
+
+def load_df_from_dir(dir_name, file_name, expected_n=1):
+    if expected_n > 1:
+        all_dfs = []
+
+    files = list(pathlib.Path(dir_name).rglob(file_name))
+    if len(files) != expected_n:
+        raise ValueError(f'There was not the correct number of event files in the '
+                         f"directory {dir_name} and it's sub directories."
+                         f'expected {expected_n}, but got {len(file_name)}.')
+
+    for file in files:
+        csv_data = pd.read_csv(file)
+        if expected_n == 1:
+            return csv_data
+        all_dfs.append(csv_data)
+    return all_dfs
--- src/pupil_labs/invisible_lsl_relay/xdf_cloud_time_sync.py
+++ src/pupil_labs/invisible_lsl_relay/xdf_cloud_time_sync.py
@@ -1,7 +1,7 @@
 import json
 import logging
 import pathlib
-
+import pupil_labs.invisible_lsl_relay.linear_time_model as lm_time
 import click
 import pyxdf
 
@@ -10,7 +10,9 @@ logger = logging.getLogger(__name__)
 
 @click.command()
 @click.argument(
-    'path_to_xdf', nargs=1, type=click.Path(exists=True, file_okay=True, dir_okay=False)
+    'path_to_xdf',
+    nargs=1,
+    type=click.Path(exists=True, file_okay=True, dir_okay=False)
 )
 @click.argument(
     'paths_to_exports',
@@ -19,7 +21,12 @@ logger = logging.getLogger(__name__)
                     file_okay=False,
                     dir_okay=True),
 )
-def main(path_to_xdf, paths_to_exports):
+@click.option(
+    '--output_path',
+    default='.',
+    type=click.Path(exists=True, file_okay=False, dir_okay=True)
+)
+def main(path_to_xdf, paths_to_exports, output_path):
     # set the logging
     logging.basicConfig(
         level=logging.DEBUG,
@@ -37,46 +44,57 @@ def main(path_to_xdf, paths_to_exports):
     if len(paths_to_exports) == 0:
         logger.info('No paths to exports provided. Looking inside current directory.')
         paths_to_exports = ['.']
-    load_files(path_to_xdf, paths_to_exports)
+
+    if not output_path.endswith('/'):
+        output_path += '/'
+
+    align_and_save_data(path_to_xdf, paths_to_exports, output_path)
 
 
-def load_files(path_to_xdf, paths_to_export):
+def align_and_save_data(path_to_xdf, paths_to_cloud, output_path):
     # load the xdf file
     try:
-        xdf_head, xdf_data = pyxdf.load_xdf(path_to_xdf)
+        xdf_head, xdf_data = pyxdf.load_xdf(path_to_xdf, select_streams=[{'type': 'Gaze'}])
     except Exception:
         raise OSError(f"Invalid xdf file {path_to_xdf}")
     # extract all serial numbers from gaze streams
     xdf_serial_nums = extract_serial_num_from_xdf(xdf_head)
     # check cloud export paths
-    for path in paths_to_export:
+    for path in paths_to_cloud:
         for info_path in pathlib.Path(path).rglob("info.json"):
-            if json_serial_in_xdf(info_path, xdf_serial_nums):
-                perform_time_alignment(path_to_xdf, info_path.parent)
-
-
-def perform_time_alignment(xdf_path, recording_dir):
-    print(f'perform time alignment between {xdf_path} and {recording_dir}')
-    save_files()
+            serial_num_found_in_xdf, serial_num = json_serial_in_xdf(
+                info_path, xdf_serial_nums
+            )
+            if serial_num_found_in_xdf:
+                logger.debug(f'found serial number {serial_num}')
+                cloud_aligned_time, cloud_to_lsl_mapper,\
+                    lsl_to_cloud_mapper = lm_time.perform_time_alignment(
+                        path_to_xdf,
+                        info_path.parent,
+                        serial_num)
+                save_files(cloud_aligned_time,
+                           cloud_to_lsl_mapper,
+                           lsl_to_cloud_mapper,
+                           output_path)
 
 
 def json_serial_in_xdf(path_infojson, xdf_serial_nums):
     with open(path_infojson) as infojson:
         data_infojson = json.load(infojson)
-        if extract_serial_num_from_infojson(data_infojson) in xdf_serial_nums:
-            return True
-    return False
+        serial_infojson = extract_serial_num_from_infojson(data_infojson)
+        if serial_infojson in xdf_serial_nums:
+            return True, serial_infojson
+    return False, None
 
 
 def extract_serial_num_from_xdf(xdf_head):
     camera_serial_nums = []
     for x in xdf_head:
         try:
-            if x['info']['type'][0] == 'Gaze':
-                xdf_camera_serial = x['info']['desc'][0]['acquisition'][0][
-                    'world_camera_serial'
-                ][0]
-                camera_serial_nums.append(xdf_camera_serial)
+            xdf_camera_serial = x['info']['desc'][0]['acquisition'][0][
+                'world_camera_serial'
+            ][0]
+            camera_serial_nums.append(xdf_camera_serial)
         except KeyError as e:
             raise KeyError(
                 "The xdf file does not contain the expected fields. "
@@ -95,9 +113,31 @@ def extract_serial_num_from_infojson(data_infojson):
         return None
 
 
-def save_files():
-    pass
+def save_files(cloud_aligned_time, cloud_to_lsl, lsl_to_cloud, output_path):
+    cloud_aligned_time.to_csv(output_path + 'gaze.csv')
+
+    mapping_parameters = write_mapper_to_file(cloud_to_lsl, lsl_to_cloud)
+    with open(output_path + 'parameters.json', 'w') as out_file:
+        json.dump(mapping_parameters, out_file, indent=4)
+
+
+def write_mapper_to_file(cloud_to_lsl, lsl_to_cloud):
+    mapping_parameters = {
+        'cloud_to_lsl': {
+            'intercept': cloud_to_lsl.intercept_,
+            'slope': cloud_to_lsl.coef_[0]
+        },
+        'lsl_to_cloud': {
+            'intercept': lsl_to_cloud.intercept_,
+            'slope': lsl_to_cloud.coef_[0]
+        },
+        'info': {
+            'model_type': type(cloud_to_lsl).__name__,
+        },
+    }
+    return mapping_parameters
 
 
 if __name__ == '__main__':
     main()
+
