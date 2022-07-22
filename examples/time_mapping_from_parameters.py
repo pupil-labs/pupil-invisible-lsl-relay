57fee3028fa0e7526b091c1e1a5e23755988ef27
Clara Kuper <clara-kuper@web.de>
2022-07-22 14:59:17 +0200

    fix typos in documentation

--- examples/time_mapping_from_parameters.py
+++ examples/time_mapping_from_parameters.py
@@ -1,5 +1,6 @@
 # imports for the full pipeline
 import json
+
 import pandas as pd
 
 column_timestamp = 'timestamp [s]'
@@ -15,13 +16,14 @@ with open('./recordings/time_alignment_parameters.json') as file:
     parameter_dict = json.load(file)
 
 
+# define a simple linear model
 def perform_linear_mapping(input_data, parameters):
     return parameters['intercept'] + input_data * parameters['slope']
 
 
 # predict lsl time in seconds
 cloud_gaze_data['lsl_time [s]'] = perform_linear_mapping(
-    cloud_gaze_data[column_timestamp],
-    parameter_dict['cloud_to_lsl'])
+    cloud_gaze_data[column_timestamp], parameter_dict['cloud_to_lsl']
+)
 
 cloud_gaze_data.to_csv('./recordings/time_aligned_gaze.csv')
